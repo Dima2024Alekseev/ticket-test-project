c13d83aa5d7d7a95be4038dad3d5e767f6e5443e
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-08-17 22:40:48 +0700

    done

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import "./style.ccs";
+import "./style.css";
 
 const Register = () => {
     const [email, setEmail] = useState('');
