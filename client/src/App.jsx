07ad0fd8334853205a786fd27bdd96b8585e9be9
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-08-17 19:24:01 +0700

    done

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../src/styles/index.css";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Привет, сучки!!!</h1>
+      </div>
+    </>
+  )
+}
+
+
+export default App;
